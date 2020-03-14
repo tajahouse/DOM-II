519a141c1f005617fada7430c6961d382525991a
Taja House <ahlliahtaja@yahoo.com>
2020-03-14 15:14:36 -0700

    centered heading on content for main page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
+//Navigation bar
 const onHover = event => event.target.style.color = "orange";
 const navBar = document.querySelectorAll('a');
 navBar.forEach(anchors =>{
     anchors.addEventListener('mouseover', onHover)
 });
 navBar.forEach(anchors =>{anchors.addEventListener('mouseleave', (event)=>{event.target.style.color = "black"})});
+
+//styling for h2 content
+const hTwo = document.querySelectorAll('h2');
+hTwo.forEach(heading =>{
+    heading.style.textAlign = "center";
+})
