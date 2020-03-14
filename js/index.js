c199770761ab018f2b6454465042a5d9d9e02e72
Taja House <ahlliahtaja@yahoo.com>
2020-03-14 15:09:33 -0700

    added on hover to change nav bar orange

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+const onHover = event => event.target.style.color = "orange";
+const navBar = document.querySelectorAll('a');
+navBar.forEach(anchors =>{
+    anchors.addEventListener('mouseover', onHover)
+});
+navBar.forEach(anchors =>{anchors.addEventListener('mouseleave', (event)=>{event.target.style.color = "black"})});
